69d4b50e2a98de5f6346b79f3c38cf09bc9609a5
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-03 23:08:38 +0200

    Filtering through fetch request result using an NS predicate!

--- UnderstandingCoreData/ContentView.swift
+++ UnderstandingCoreData/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star wars'")) var ships: FetchedResults<Ship>
+ 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            List(ships, id: \.self) {ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Start trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Difieant"
+                ship2.universe = "Star trek"
+                
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star wars"
+                
+                try? moc.save()
+            }
         }
-        .padding()
+        
     }
 }
 
