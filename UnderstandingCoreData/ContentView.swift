107ce585389e69c1071d2f74c070da6a0dab6a57
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-03 23:22:51 +0200

    creating the objects of the singers.

--- UnderstandingCoreData/ContentView.swift
+++ UnderstandingCoreData/ContentView.swift
@@ -9,34 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    
-    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star wars'")) var ships: FetchedResults<Ship>
+    @State private var lastNameFilter = "A"
+   
  
     var body: some View {
-        VStack{
-            List(ships, id: \.self) {ship in
-                Text(ship.name ?? "Unknown name")
-            }
-            
+       
+        VStack {
+            //list of matching singers
             Button("Add Examples") {
-                let ship1 = Ship(context: moc)
-                ship1.name = "Enterprise"
-                ship1.universe = "Start trek"
+                let taylor = Singer(context: moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
                 
-                let ship2 = Ship(context: moc)
-                ship2.name = "Difieant"
-                ship2.universe = "Star trek"
+                let ed = Singer(context: moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
                 
-                
-                let ship3 = Ship(context: moc)
-                ship3.name = "Millennium Falcon"
-                ship3.universe = "Star wars"
-                
-                let ship4 = Ship(context: moc)
-                ship4.name = "Executor"
-                ship4.universe = "Star wars"
+                let adele = Singer(context: moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
                 
                 try? moc.save()
+            }
+            
+            Button("Show A") {
+                lastNameFilter = "S"
             }
         }
         
