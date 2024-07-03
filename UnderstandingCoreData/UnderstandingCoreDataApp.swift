e874509cdad25d403e46e88c3174d7558eeb6158
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-03 15:26:42 +0200

    Initial Commit

---
+++ UnderstandingCoreData/UnderstandingCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingCoreDataApp.swift
+//  UnderstandingCoreData
+//
+//  Created by Buhle Radzilani on 2024/07/03.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
