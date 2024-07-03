af3673b304c5d85fac257e95aa17d78ebc5f895c
buhleradzilani <radzilanibuhle@gmail.com>
2024-07-03 23:16:37 +0200

    creating the sub classes

---
+++ UnderstandingCoreData/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  UnderstandingCoreData
+//
+//  Created by Buhle Radzilani on 2024/07/03.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
